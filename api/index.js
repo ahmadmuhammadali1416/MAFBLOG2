6f874108f5b3a6b4c852a024278ec1171a3f3ef4
MAF1-blog <ahmadmuhammadali1416@gmail.com>
2024-08-05 12:33:36 -0700

    till redux

---
+++ api/index.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+
+dotenv.config();
+
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Mongodb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World......");
+});
+
+
+app.use("/api/user/", userRoutes);
+
+
+app.use(express.json()); // this is going to allow json as the input of the backend.
+app.use('/api/auth', authRoutes)
+
+app.use((err, req, res, next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success : false,
+    statusCode,
+    message
+  });
+});
+
+app.use((err, req, res, next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success : false,
+    statusCode,
+    message
+  });
+});
+
+
+
+
---
+++ api/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+
+//After creating Schema, we need to create Model
+const User = mongoose.model("User", userSchema); //Name should be capital, We don't put 's' because Mongodb 
+//is going to  add it automatically.
+
+
+export default User; // export it default so we can use it in other places in our application,
+// when we need it. so model is created now, we'll use it later when we are Signing up user or update them.
